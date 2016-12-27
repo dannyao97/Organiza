f949198ea34981f279ea270e513506a2a513b635
Daniel <Daniel@DanDanOrangeBox>
2016-12-26 16:42:48 -0800

    Table can select cells

--- src/Organiza/FXMLDocumentController.java
+++ src/Organiza/FXMLDocumentController.java
@@ -4,8 +4,13 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTreeTableView;
 import com.jfoenix.controls.cells.editors.base.JFXTreeTableCell;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,7 +18,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -39,12 +46,13 @@ public class FXMLDocumentController implements Initializable
     @FXML
     private JFXButton btnMovies;
     @FXML
+    private JFXButton btnTv;
+    @FXML
     private ScrollPane scrollMovies;
     @FXML
     private ScrollPane scrollTV;
     @FXML
-    private TableView tableMovies;
-
+    private TableView<TableImage> tableMovies;
     private static Stage stage;
     private Mouse mouse;
     private static double xOffset = 0;
@@ -105,15 +113,96 @@ public class FXMLDocumentController implements Initializable
 
     public void setTable()
     {
-        TableColumn<TableImage, ImageView> col = new TableColumn<>();
-        tableMovies.getColumns().add(col);
-        
-        imageList.add(new TableImage(new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg")));
-        col.setPrefWidth(50);
-        col.setCellValueFactory(new PropertyValueFactory<TableImage, ImageView>("image"));
+        scrollMovies.setFitToWidth(true);
+        tableMovies.getSelectionModel().setCellSelectionEnabled(true);
+        tableMovies.getSelectionModel().getSelectedCells().addListener(new ListChangeListener<TablePosition>()
+        {
+            @Override
+            public void onChanged(Change change)
+            {
+                for (TablePosition pos : tableMovies.getSelectionModel().getSelectedCells())
+                {
+                    System.out.println("Cell selected in row " + pos.getRow() + " and column " + pos.getColumn());
+                }
+            }
+        });
+        /*tableMovies.getSelectionModel().selectedItemProperty().addListener(new ChangeListener()
+        {
+            @Override
+            public void changed(ObservableValue observableValue, Object oldValue, Object newValue)
+            {
+                System.out.println(observableValue);
+                //Check whether item is selected and set value of selected item to Label
+                if (tableMovies.getSelectionModel().getSelectedItem() != null)
+                {
+                    TableViewSelectionModel selectionModel = tableMovies.getSelectionModel();
+                    ObservableList selectedCells = selectionModel.getSelectedCells();
+                    TablePosition tablePosition = (TablePosition) selectedCells.get(0);
+                    Object val = tablePosition.getTableColumn().getCellData(newValue);
+                    System.out.println("Selected Value" + val);
+                    System.out.println("row: " + tablePosition.getRow() +"\ncol: " + tablePosition.getColumn());
+                }
+            }
+        });*/
 
-        int i = 0;
-        while (i != 10)
+        TableColumn<TableImage, ImageView> col1 = new TableColumn<>("col1");
+        ImageView img1 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg");
+        img1.setPreserveRatio(true);
+        img1.setFitWidth(170);
+        col1.setPrefWidth(img1.getFitWidth() + 25);
+        col1.setStyle("-fx-background-color: #2c2c2c;");
+        col1.setCellValueFactory(new PropertyValueFactory<TableImage, ImageView>("image1"));
+
+        TableColumn<TableImage, ImageView> col2 = new TableColumn<>("col2");
+        ImageView img2 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMTEyMDg4MDU4MjdeQTJeQWpwZ15BbWU4MDQyOTc3MjIx._V1_SX300.jpg");
+        img2.setPreserveRatio(true);
+        img2.setFitWidth(170);
+        col2.setPrefWidth(img2.getFitWidth() + 25);
+        col2.setStyle("-fx-background-color: #2c2c2c;");
+        col2.setCellValueFactory(new PropertyValueFactory<TableImage, ImageView>("image2"));
+
+        TableColumn<TableImage, ImageView> col3 = new TableColumn<>("col3");
+        ImageView img3 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjE1MDYxOTA4MF5BMl5BanBnXkFtZTcwMDE0MDUzMw@@._V1_SX300.jpg");
+        img3.setPreserveRatio(true);
+        img3.setFitWidth(170);
+        col3.setPrefWidth(img3.getFitWidth() + 25);
+        col3.setStyle("-fx-background-color: #2c2c2c;");
+        col3.setCellValueFactory(new PropertyValueFactory<TableImage, ImageView>("image3"));
+
+        TableColumn<TableImage, ImageView> col4 = new TableColumn<>("col4");
+        ImageView img4 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BNTEyMjAwMDU1OV5BMl5BanBnXkFtZTcwNDQyNTkxMw@@._V1_SX300.jpg");
+        img4.setPreserveRatio(true);
+        img4.setFitWidth(170);
+        col4.setPrefWidth(img4.getFitWidth() + 25);
+        col4.setStyle("-fx-background-color: #2c2c2c;");
+        col4.setCellValueFactory(new PropertyValueFactory<TableImage, ImageView>("image4"));
+
+        TableColumn<TableImage, ImageView> col5 = new TableColumn<>("col5");
+        ImageView img5 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX300.jpg");
+        img5.setPreserveRatio(true);
+        img5.setFitWidth(170);
+        col5.setPrefWidth(img5.getFitWidth() + 25);
+        col5.setStyle("-fx-background-color: #2c2c2c;");
+        col5.setCellValueFactory(new PropertyValueFactory<TableImage, ImageView>("image5"));
+
+        ImageView img6 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg");
+        img6.setPreserveRatio(true);
+        img6.setFitWidth(170);
+        ImageView img7 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg");
+        img7.setPreserveRatio(true);
+        img7.setFitWidth(170);
+        ImageView img8 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg");
+        img8.setPreserveRatio(true);
+        img8.setFitWidth(170);
+        ImageView img9 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg");
+        img9.setPreserveRatio(true);
+        img9.setFitWidth(170);
+        ImageView img10 = new ImageView("https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg");
+        img10.setPreserveRatio(true);
+        img10.setFitWidth(170);
+
+        /*int i = 0;
+        while (i != 1)
         {
 
             try
@@ -123,8 +212,25 @@ public class FXMLDocumentController implements Initializable
             {
             }
             i++;
-        }
+        }*/
+        tableMovies.getColumns().addAll(col1, col2, col3, col4, col5);
+        imageList.add(new TableImage(img1, img2, img3, img4, img5));
+        imageList.add(new TableImage(null, null, null, null, null));
+        imageList.add(new TableImage(img6, img7, img8, img9, img10));
         tableMovies.setItems(imageList);
+
+        Pane header = (Pane) tableMovies.lookup("TableHeaderRow");
+        if (header != null && header.isVisible())
+        {
+            header.setMaxHeight(0);
+            header.setMinHeight(0);
+            header.setPrefHeight(0);
+            header.setVisible(false);
+            header.setManaged(false);
+        }
+
+        //tableMovies.setStyle("visibility: hidden; -fx-padding: -1em;");
+        //tableMovies.getColumns().addAll(colList);
     }
 
     public void setStage(final Stage stage)
--- src/Organiza/TableImage.java
+++ src/Organiza/TableImage.java
@@ -5,20 +5,73 @@ import javafx.scene.image.ImageView;
 public class TableImage
 {
 
-    private ImageView image;
+    private ImageView image1;
+    private ImageView image2;
+    private ImageView image3;
+    private ImageView image4;
+    private ImageView image5;
 
-    public TableImage(ImageView image)
+    public TableImage(ImageView image1, ImageView image2, ImageView image3, ImageView image4, ImageView image5)
     {
-        this.image = image;
+        this.image1 = image1;
+        this.image2 = image2;
+        this.image3 = image3;
+        this.image4 = image4;
+        this.image5 = image5;
     }
 
-    public ImageView getImage()
+    //Get Set Image 1
+    public ImageView getImage1()
     {
-        return image;
+        return image1;
     }
 
-    public void setImage(ImageView image)
+    public void setImage1(ImageView image1)
     {
-        this.image = image;
+        this.image1 = image1;
+    }
+
+    //Get Set Image 2
+    public ImageView getImage2()
+    {
+        return image2;
+    }
+
+    public void setImage2(ImageView image2)
+    {
+        this.image2 = image2;
+    }
+
+    //Get Set Image 3
+    public ImageView getImage3()
+    {
+        return image3;
+    }
+
+    public void setImage3(ImageView image3)
+    {
+        this.image3 = image3;
+    }
+
+    //Get Set Image 4
+    public ImageView getImage4()
+    {
+        return image4;
+    }
+
+    public void setImage4(ImageView image4)
+    {
+        this.image4 = image4;
+    }
+
+    //Get Set Image 5
+    public ImageView getImage5()
+    {
+        return image5;
+    }
+
+    public void setImage5(ImageView image5)
+    {
+        this.image5 = image5;
     }
 }
